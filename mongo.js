3b9e4614f79128fbff99f21b77d8b859b785370f
Exeiya <krista.lindholm@gmail.com>
2018-11-15 22:27:52 +0200

    week 3 exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const url = process.env.MONGODB_URI
+mongoose.connect(url, { useNewUrlParser: true })
+
+const Person = mongoose.model('Person', {
+  name: String,
+  number: String
+})
+
+if (process.argv.length === 4) {
+  const person = new Person({
+    name: process.argv[2],
+    number: process.argv[3]
+  })
+
+  person
+    .save()
+    .then(savedPerson => {
+      console.log(`lisätään henkilö ${savedPerson.name} numero ${savedPerson.number} luetteloon`)
+      mongoose.connection.close()
+    })
+} else {
+  console.log('Puhelinluettelo:')
+  Person
+    .find({})
+    .then(persons => {
+      persons.forEach(person => {
+        console.log(`${person.name} ${person.number}`)
+      })
+      mongoose.connection.close()
+    })
+}
